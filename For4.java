eab449c0a3b5f506763bb0a0cba368a66c852f00
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-01-09 14:52:30 +0530

    Delete For4.java

--- For4.java
+++
@@ -1,9 +0,0 @@
-//Printing the table of number give by the user
-void main()
-{
-	int num=Integer.parseInt(IO.readln("Enter a number:"));
-	for(int i=1;i<=10;i++)
-	{
-		IO.println(num+"X"+i+"="+(num*i));
-	}
-}
